09060010bc97cec197ce489b325ccb604fadff32
Shivang <shivang.iitk@gmail.com>
2025-04-27 02:27:18 -0700

    Panel Class optimised

--- composeApp/src/commonMain/kotlin/ai/rever/boss/v4/components/model/Panel.kt
+++ composeApp/src/commonMain/kotlin/ai/rever/boss/v4/components/model/Panel.kt
@@ -70,149 +70,24 @@ open class Panel() {
             else -> this
         }
 
-        val TOP.top: TOP
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.top)
-                is LEFT -> this.copy(panel = this.panel.top)
-                is RIGHT -> this.copy(panel = this.panel.top)
-                is BOTTOM -> this.copy(panel = this.panel.top)
-                else -> this.copy(panel = Panel.top)
-            }
+        val TOP.top: TOP get() = this.copy(panel = this.panel?.top ?: Panel.top)
+        val TOP.left: TOP get() = this.copy(panel = this.panel?.left ?: Panel.left)
+        val TOP.right: TOP get() = this.copy(panel = this.panel?.right ?: Panel.right)
+        val TOP.bottom: TOP get() = this.copy(panel = this.panel?.bottom ?: Panel.bottom)
 
-        val TOP.left: TOP
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.left)
-                is LEFT -> this.copy(panel = this.panel.left)
-                is RIGHT -> this.copy(panel = this.panel.left)
-                is BOTTOM -> this.copy(panel = this.panel.left)
-                else -> this.copy(panel = Panel.left)
-            }
+        val LEFT.top: LEFT get() = this.copy(panel = this.panel?.top ?: Panel.top)
+        val LEFT.left: LEFT get() = this.copy(panel = this.panel?.left ?: Panel.left)
+        val LEFT.right: LEFT get() = this.copy(panel = this.panel?.right ?: Panel.right)
+        val LEFT.bottom: LEFT get() = this.copy(panel = this.panel?.bottom ?: Panel.bottom)
 
-        val TOP.right: TOP
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.right)
-                is LEFT -> this.copy(panel = this.panel.right)
-                is RIGHT -> this.copy(panel = this.panel.right)
-                is BOTTOM -> this.copy(panel = this.panel.right)
-                else -> this.copy(panel = Panel.right)
-            }
+        val RIGHT.top: RIGHT get() = this.copy(panel = this.panel?.top ?: Panel.top)
+        val RIGHT.left: RIGHT get() = this.copy(panel = this.panel?.left ?: Panel.left)
+        val RIGHT.right: RIGHT get() = this.copy(panel = this.panel?.right ?: Panel.right)
+        val RIGHT.bottom: RIGHT get() = this.copy(panel = this.panel?.bottom ?: Panel.bottom)
 
-        val TOP.bottom: TOP
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.bottom)
-                is LEFT -> this.copy(panel = this.panel.bottom)
-                is RIGHT -> this.copy(panel = this.panel.bottom)
-                is BOTTOM -> this.copy(panel = this.panel.bottom)
-                else -> this.copy(panel = Panel.bottom)
-            }
-
-        val LEFT.top: LEFT
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.top)
-                is LEFT -> this.copy(panel = this.panel.top)
-                is RIGHT -> this.copy(panel = this.panel.top)
-                is BOTTOM -> this.copy(panel = this.panel.top)
-                else -> this.copy(panel = Panel.top)
-            }
-
-        val LEFT.left: LEFT
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.left)
-                is LEFT -> this.copy(panel = this.panel.left)
-                is RIGHT -> this.copy(panel = this.panel.left)
-                is BOTTOM -> this.copy(panel = this.panel.left)
-                else -> this.copy(panel = Panel.left)
-            }
-
-        val LEFT.right: LEFT
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.right)
-                is LEFT -> this.copy(panel = this.panel.right)
-                is RIGHT -> this.copy(panel = this.panel.right)
-                is BOTTOM -> this.copy(panel = this.panel.right)
-                else -> this.copy(panel = Panel.right)
-            }
-
-        val LEFT.bottom: LEFT
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.bottom)
-                is LEFT -> this.copy(panel = this.panel.bottom)
-                is RIGHT -> this.copy(panel = this.panel.bottom)
-                is BOTTOM -> this.copy(panel = this.panel.bottom)
-                else -> this.copy(panel = Panel.bottom)
-            }
-
-        val RIGHT.top: RIGHT
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.top)
-                is LEFT -> this.copy(panel = this.panel.top)
-                is RIGHT -> this.copy(panel = this.panel.top)
-                is BOTTOM -> this.copy(panel = this.panel.top)
-                else -> this.copy(panel = Panel.top)
-            }
-
-        val RIGHT.left: RIGHT
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.left)
-                is LEFT -> this.copy(panel = this.panel.left)
-                is RIGHT -> this.copy(panel = this.panel.left)
-                is BOTTOM -> this.copy(panel = this.panel.left)
-                else -> this.copy(panel = Panel.left)
-            }
-
-        val RIGHT.right: RIGHT
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.right)
-                is LEFT -> this.copy(panel = this.panel.right)
-                is RIGHT -> this.copy(panel = this.panel.right)
-                is BOTTOM -> this.copy(panel = this.panel.right)
-                else -> this.copy(panel = Panel.right)
-            }
-
-        val RIGHT.bottom: RIGHT
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.bottom)
-                is LEFT -> this.copy(panel = this.panel.bottom)
-                is RIGHT -> this.copy(panel = this.panel.bottom)
-                is BOTTOM -> this.copy(panel = this.panel.bottom)
-                else -> this.copy(panel = Panel.bottom)
-            }
-
-        val BOTTOM.top: BOTTOM
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.top)
-                is LEFT -> this.copy(panel = this.panel.top)
-                is RIGHT -> this.copy(panel = this.panel.top)
-                is BOTTOM -> this.copy(panel = this.panel.top)
-                else -> this.copy(panel = Panel.top)
-            }
-
-        val BOTTOM.left: BOTTOM
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.left)
-                is LEFT -> this.copy(panel = this.panel.left)
-                is RIGHT -> this.copy(panel = this.panel.left)
-                is BOTTOM -> this.copy(panel = this.panel.left)
-                else -> this.copy(panel = Panel.left)
-            }
-
-        val BOTTOM.right: BOTTOM
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.right)
-                is LEFT -> this.copy(panel = this.panel.right)
-                is RIGHT -> this.copy(panel = this.panel.right)
-                is BOTTOM -> this.copy(panel = this.panel.right)
-                else -> this.copy(panel = Panel.right)
-            }
-
-        val BOTTOM.bottom: BOTTOM
-            get() = when (this.panel) {
-                is TOP -> this.copy(panel = this.panel.bottom)
-                is LEFT -> this.copy(panel = this.panel.bottom)
-                is RIGHT -> this.copy(panel = this.panel.bottom)
-                is BOTTOM -> this.copy(panel = this.panel.bottom)
-                else -> this.copy(panel = Panel.bottom)
-            }
+        val BOTTOM.top: BOTTOM get() = this.copy(panel = this.panel?.top ?: Panel.top)
+        val BOTTOM.left: BOTTOM get() = this.copy(panel = this.panel?.left ?: Panel.left)
+        val BOTTOM.right: BOTTOM get() = this.copy(panel = this.panel?.right ?: Panel.right)
+        val BOTTOM.bottom: BOTTOM get() = this.copy(panel = this.panel?.bottom ?: Panel.bottom)
     }
 }
-
